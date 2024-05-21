6a66c01e816aa26df6d3a4c2ce85320f6d127dbe
francisc <loghinfrancisc@gmail.com>
2024-05-21 20:46:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# AssistBot
+
+Welcome to AssistBot, a customer support chatbot that leverages a fine-tuned version of OpenAI's GPT-3.5-turbo to generate accurate and context-aware responses.
+
+## Features
+
+- **Interactive User Interface:** Built with JavaScript for a seamless and intuitive user experience.
+- **Real-time Database:** Uses Firebase for efficient and scalable database storage.
+- **AI-Powered Support:** Integrates OpenAI's API with a fine-tuned GPT-3.5-turbo model to deliver high-quality customer support.
+- **Rich Training Dataset:** The model is fine-tuned using the Bitext - Customer Service Training Dataset, available on HuggingFace, ensuring comprehensive and relevant assistance.
+
+## Technologies Used
+
+- **JavaScript:** For creating the dynamic and interactive front-end interface.
+- **Firebase:** For backend database storage and management.
+- **OpenAI API:** To integrate and fine-tune the GPT-3.5-turbo model for customer support functionality.
+- **Vite:** For building and serving the application efficiently.
+
+If you want to test the app you will have to create your own fine-tuned AI model as currently OpenAI does not allow sharing of custom models. Here is a simplified instruction on how to set up the project.
+
+## Getting Started
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/loghinfrancisc/AssistBot.git
+   cd AssistBot
+
+2. **Install Dependencies:**
+Ensure you have 'npm' installed, Then run:
+    ```bash
+    npm install
+
+3. **Set up Firebase:**
+Create a Firebase project and add your configuration details in the firebaseConfig variable.
+
+4. **Configure OpenAI API:**
+Obtain an API key from OpenAI and add it to your '.env' file.
+
+5. **Run the Application:**
+    ```bash
+    npm start
+
+## Usage
+
+Once the app is runing, navigate to 'http://localhost:3000' in your browser to interact with the ChatGPT-powered customer support assistant.
---
+++ index.js
@@ -0,0 +1,152 @@
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, push, get, remove } from 'firebase/database'
+
+import OpenAI from 'openai'
+// import { process } from './env'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true
+})
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCDgWZsqii17KgmVfNe3Wpyc525FJDdp8E",
+    authDomain: "openai-chatbot-6d22b.firebaseapp.com",
+    databaseURL: "https://openai-chatbot-6d22b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "openai-chatbot-6d22b",
+    storageBucket: "openai-chatbot-6d22b.appspot.com",
+    messagingSenderId: "878339113334",
+    appId: "1:878339113334:web:dbb5c7ed8e3d2073333a57"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getDatabase(app)
+const conversationInDb = ref(db)
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+
+const instructionObj = {
+    "role": "system",
+    "content": "You're a very knowledgeable assistant that is always eager to help."
+}
+
+document.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input')
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    try {
+        await pushUserMessage(userInput.value)
+        userInput.value = ''
+        await fetchReply()
+    } catch (error) {
+        console.error('Error:', error.message)
+    }
+
+})
+
+document.getElementById('clear-btn').addEventListener('click', async () => {
+    try {
+        await clearConversation()
+    } catch (error) {
+        console.error('Error:', error.message)
+    }
+})
+
+async function pushUserMessage(content) {
+    try {
+        await push(conversationInDb, {
+            "role": "user",
+            "content": content
+        })
+    } catch (error) {
+        console.error('Error pushing user message:', error.message)
+        throw error // Re-throw the error for upstream handling if needed
+    }
+}
+
+async function clearConversation() {
+    try {
+        await remove(conversationInDb)
+        chatbotConversation.innerHTML = '<div class="speech speech-ai">How can I help you?</div>'
+    } catch (error) {
+        console.error('Error clearing conversation:', error.message)
+        throw error // Re-throw the error for upstream handling if needed
+    }
+}
+
+async function fetchReply() {
+    try {
+        const snapshot = await get(conversationInDb)
+        if (snapshot.exists()) {
+            const conversationArray = Object.values(snapshot.val())
+            conversationArray.unshift(instructionObj)
+            const response = await openai.chat.completions.create({
+                messages: conversationArray,
+                model: "ft:gpt-3.5-turbo-0125:personal::9RMdu0if",
+                presence_penalty: 0,
+                frequency_penalty: 0.1,
+                temperature: 0.2
+            })
+            await push(conversationInDb, response.choices[0].message)
+            renderTypewriterText(response.choices[0].message.content)
+        } else {
+            console.log("No data available");
+        }
+    } catch (error) {
+        console.error('Error fetching reply:', error.message)
+    }
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    let isManuallyScrolling = false
+
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i-1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        if (!isManuallyScrolling) {
+            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+        }
+    }, 50);
+
+    // Event listener to detect manual scrolling
+    chatbotConversation.addEventListener('scroll', () => {
+        isManuallyScrolling = chatbotConversation.scrollTop < chatbotConversation.scrollHeight - chatbotConversation.offsetHeight;
+    });
+}
+
+function renderConversationFromDB() {
+    get(conversationInDb).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            Object.values(snapshot.val()).forEach(message => {
+                const newSpeechBubble = document.createElement('div')
+                newSpeechBubble.classList.add(
+                    'speech',
+                    `speech-${message.role === 'user' ? 'human' : 'ai'}`
+                )
+                chatbotConversation.appendChild(newSpeechBubble)
+                newSpeechBubble.textContent = message.content
+            })
+            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+        }
+        else {
+            console.log("No data available");
+        }
+    }).catch(error => {
+        console.error('Error rendering conversation from DB:', error.message)
+    })
+}
+
+renderConversationFromDB()
