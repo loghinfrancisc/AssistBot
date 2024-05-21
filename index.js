0d6f27c4ed6673e4576b14e0c51d1df08df7418f
francisc <loghinfrancisc@gmail.com>
2024-05-21 21:55:50 +0200

    Making changes for netlify serverless function.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, push, get, remove } from 'firebase/database'
-
-import OpenAI from 'openai'
-// import { process } from './env'
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true
-})
+//
+// import OpenAI from 'openai'
+// // import { process } from './env'
+//
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true
+// })
 
 const firebaseConfig = {
     apiKey: "AIzaSyCDgWZsqii17KgmVfNe3Wpyc525FJDdp8E",
@@ -81,19 +81,32 @@ async function clearConversation() {
 
 async function fetchReply() {
     try {
+
         const snapshot = await get(conversationInDb)
         if (snapshot.exists()) {
             const conversationArray = Object.values(snapshot.val())
             conversationArray.unshift(instructionObj)
-            const response = await openai.chat.completions.create({
-                messages: conversationArray,
-                model: "ft:gpt-3.5-turbo-0125:personal::9RMdu0if",
-                presence_penalty: 0,
-                frequency_penalty: 0.1,
-                temperature: 0.2
+
+            const url = 'https://glistening-sfogliatella-2343da.netlify.app/.netlify/functions/fetchAI'
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+                body: conversationArray
             })
-            await push(conversationInDb, response.choices[0].message)
-            renderTypewriterText(response.choices[0].message.content)
+            const data = await response.json()
+            console.log(data)
+
+            // const response = await openai.chat.completions.create({
+            //     messages: conversationArray,
+            //     model: "ft:gpt-3.5-turbo-0125:personal::9RMdu0if",
+            //     presence_penalty: 0,
+            //     frequency_penalty: 0.1,
+            //     temperature: 0.2
+            // })
+            // await push(conversationInDb, response.choices[0].message)
+            // renderTypewriterText(response.choices[0].message.content)
         } else {
             console.log("No data available");
         }
